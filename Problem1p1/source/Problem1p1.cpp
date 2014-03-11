0cc9f875dfea688bceb15b1b7572d9a176256292
Ryan Kelley <kelleyrw@gmail.com>
2014-03-10 17:27:14 -0700

    Added example table

--- Problem1p1/source/Problem1p1.cpp
+++ Problem1p1/source/Problem1p1.cpp
@@ -3,5 +3,13 @@
 int main()
 {
     SimpleTable table;
+    table.useTitle();
+    table.setTitle("test table1");
+    table.setTable() 
+        ("c1", "c2", "c3")
+        ("v1",    1,    2)
+        ("v2",    1,    2)
+        ;
+    table.print();
     return 0;
 }
