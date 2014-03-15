fec31e550eff00f19e5f6488081e2731922f99d7
Ryan Kelley <kelleyrw@gmail.com>
2014-03-15 15:55:18 -0700

    First working insertion sort

--- Chapter2/source/main.cpp
+++ Chapter2/source/main.cpp
@@ -4,49 +4,75 @@
 // helper functions
 // ------------------------------------------------------------ //
 
-template <typename T, unsigned int N>
-void PrintArray(std::ostream& out, const T(&array)[N])
-{
-    if (N == 0) { out << "{}"; }
-    for (std::size_t i = 0; i != N; i++)
-    {
-        if (i == 0) { out << "{"; }
-        out << array[i];
-        if (i != (N - 1)) { out << ", "; }
-        if (i == (N - 1)) { out << "}"; }
-    }
-    return;
-}
+#include <string>
+#include <sstream>
 
 template <typename T, unsigned int N>
-std::ostream& operator << (std::ostream& out, const T(&array)[N])
+std::string ArrayString(const T(&array)[N])
 {
-    if (N == 0) { out << "{}"; }
+    std::stringstream os;
+    if (N == 0) { os << "{}"; }
     for (std::size_t i = 0; i != N; i++)
     {
-        if (i == 0) { out << "{"; }
-        out << array[i];
-        if (i != (N - 1)) { out << ", "; }
-        if (i == (N - 1)) { out << "}"; }
+        if (i == 0) { os << "{"; }
+        os << array[i];
+        if (i != (N - 1)) { os << ", "; }
+        if (i == (N - 1)) { os << "}"; }
     }
-    return out;
+    return os.str();
 }
 
 // ------------------------------------------------------------ //
 // Section 2.1 -- Insertion Sort
 // ------------------------------------------------------------ //
 
+namespace rwk
+{
+    //template <typename RandomAccessIterator>
+    //void sort(RandomAccessIterator first, RandomAccessIterator end)
+    //{
+    //    std::size_t N = std::distance(first, last);
+
+    //}
+
+    // sort in place
+    void sort(int v[], const std::size_t N)
+    {
+        for (std::size_t j = 1; j != N; j++)
+        {
+            const int key = v[j];
+            int i = j - 1;
+            while (i > -1 && v[i] > key)
+            {
+                v[i + 1] = v[i];
+                i = i - 1;
+            }
+            v[i + 1] = key;
+        }
+        return;
+    }
+
+} // namespace rwk
+
+
 void TestInsertionSort()
 {
-    const int v[] = { 5, 2, 4, 6, 1, 3 };
+    int v[] = { 5, 2, 4, 6, 1, 3 };
     const std::size_t N = sizeof(v) / sizeof(v[0]);
-    PrintArray(std::cout, v);
-    std::cout << std::endl;
-    //std::cout << v << "\n";
+    std::cout << "before sorted = " << ArrayString(v) << std::endl;
+    rwk::sort(v, N);
+    std::cout << "after sorted  = " << ArrayString(v) << std::endl;
 }
+
+// ------------------------------------------------------------ //
+// main
+// ------------------------------------------------------------ //
 
 int main()
 {
+    // section 2.1 -- insertion sort
     TestInsertionSort();
+
+
     return 0;
 }
