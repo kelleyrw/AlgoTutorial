bcc391636354f8d1eec0d9dc932f1d49b3119f21
Ryan Kelley <kelleyrw@gmail.com>
2014-03-18 09:00:09 -0700

    added problems 2.1-1 and 2.1-2

--- Chapter2/source/main.cpp
+++ Chapter2/source/main.cpp
@@ -24,17 +24,19 @@ std::string ArrayString(const T(&array)[N])
 // ------------------------------------------------------------ //
 
 #include <iostream>
+#include <functional>
 
 namespace ch2
 {
     // sort in place
-    void sort(int* const v, const std::size_t N)
+    template <typename Compare>
+    void sort(int* const v, const std::size_t N, Compare compare)
     {
         for (std::size_t j = 1; j != N; j++)
         {
             const int key = v[j];
             int i = j - 1;
-            for ( ; i > -1 && v[i] > key; i--)
+            for ( ; i > -1 && compare(key, v[i]); i--)
             {
                 v[i + 1] = v[i];
             }
@@ -43,13 +45,37 @@ namespace ch2
         return;
     }
     
+    // test the sort in place
     void TestInsertionSort()
     {
+        std::cout << "test example (page 20):\n";
         int v[] = { 5, 2, 4, 6, 1, 3 };
         const std::size_t N = sizeof(v) / sizeof(v[0]);
-        std::cout << "before sorted = " << ArrayString(v) << std::endl;
-        ch2::sort(v, N);
-        std::cout << "after sorted  = " << ArrayString(v) << std::endl;
+        std::cout << "before sorted = " << ArrayString(v) << "\n";
+        ch2::sort(v, N, std::less<int>());
+        std::cout << "after sorted  = " << ArrayString(v) << "\n" << std::endl;
+    }
+
+    // problem 2.1-1
+    void Problem2p1p1()
+    {
+        std::cout << "problem 2.1-1 (page 22):\n";
+        int v[] = { 31, 41, 59, 26, 41, 58 };
+        const std::size_t N = sizeof(v) / sizeof(v[0]);
+        std::cout << "before sorted = " << ArrayString(v) << "\n";
+        ch2::sort(v, N, std::less<int>());
+        std::cout << "after sorted  = " << ArrayString(v) << "\n" << std::endl;
+    }
+
+    // problem 2.1-2
+    void Problem2p1p2()
+    {
+        std::cout << "problem 2.1-2 (page 22):\n";
+        int v[] = { 5, 2, 4, 6, 1, 3 };
+        const std::size_t N = sizeof(v) / sizeof(v[0]);
+        std::cout << "before sorted = " << ArrayString(v) << "\n";
+        ch2::sort(v, N, std::greater<int>());
+        std::cout << "after sorted  = " << ArrayString(v) << "\n" << std::endl;
     }
 
 } // namespace ch2
@@ -63,5 +89,12 @@ int main()
 {
     // section 2.1 -- insertion sort
     ch2::TestInsertionSort();
+
+    // problem 2.1-1
+    ch2::Problem2p1p1();
+
+    // problem 2.1-2
+    ch2::Problem2p1p2();
+
     return 0;
 }
