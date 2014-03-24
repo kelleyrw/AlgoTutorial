0f278c961745dfcc0802393a7241f5130291cd54
Ryan Kelley <kelleyrw@gmail.com>
2014-03-24 13:39:06 -0700

    More iterator design changes

--- libs/r1/data_structure/include/r1/list.hpp
+++ libs/r1/data_structure/include/r1/list.hpp
@@ -13,28 +13,32 @@ namespace r1
         // member types:
         class iterator;
         class const_iterator;
-        class reverse_iterator;
-        class const_reverse_iterator;
+        typedef std::reverse_iterator<iterator> reverse_iterator;
+        typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
         typedef T value_type;
         typedef int size_type;
         typedef int difference_type;
+        typedef T& reference;
+        typedef T const& const_reference;
+        typedef T* pointer_type;
+        typedef T const* const_pointer_type;
 
         // construct:
         explicit list();
-        list(list const &rhs);
         list(list &&rhs);
+        list(list const &rhs);
         list(std::initializer_list<T> il);
 
         // assign:
-        list& operator = (list const &rhs);
         list& operator = (list &&rhs);
+        list& operator = (list const &rhs);
         list& operator = (std::initializer_list<value_type> il);
 
         // element access:
-        value_type& front();
-        value_type const& front() const;
-        value_type& back();
-        value_type const& back() const;
+        reference front();
+        const_reference front() const;
+        reference back();
+        const_reference back() const;
 
         // iterators:
         iterator begin();
@@ -52,17 +56,17 @@ namespace r1
 
         // modifiers:
         void clear();
-        iterator insert(const_iterator position, value_type const &value);
         iterator insert(const_iterator position, value_type &&value);
+        iterator insert(const_iterator position, value_type const &value);
         iterator insert(const_iterator position, std::initializer_list<value_type> il);
         template <typename InputIterator>
         iterator insert(const_iterator position, InputIterator first, InputIterator last);
         iterator erase(const_iterator position);
         iterator erase(const_iterator first, const_iterator last);
-        void push_front(value_type const &value);
         void push_front(value_type &&value);
-        void push_back(value_type const &value);
+        void push_front(value_type const &value);
         void push_back(value_type &&value);
+        void push_back(value_type const &value);
 
         // operations:
         void splice(list &l);
@@ -87,30 +91,38 @@ namespace r1
 
     // iterators
     template <typename T>
-    class list<T>::iterator : public std::iterator<std::bidirectional_iterator_tag, T>
+    class list<T>::iterator 
+        : public std::iterator
+            <
+                std::bidirectional_iterator_tag,
+                typename list::value_type,
+                typename list::difference_type,
+                typename list::pointer_type,
+                typename list::reference
+            >
     {
     public:
         // construct:
         iterator();
-        iterator(iterator const &rhs);
         iterator(iterator &&rhs);
+        iterator(iterator const &rhs);
 
         // destroy:
         ~iterator();
 
         // assign:
-        iterator& operator = (iterator const &rhs);
         iterator& operator = (iterator &&rhs);
+        iterator& operator = (iterator const &rhs);
 
         // members:
-        bool operator == (iterator const &rhs) const;
         bool operator == (iterator &&rhs) const;
-        bool operator != (iterator const &rhs) const;
+        bool operator == (iterator const &rhs) const;
         bool operator != (iterator &&rhs) const;
-        value_type& operator * ();
-        value_type const & operator * () const;
-        value_type * const operator -> ();
-        value_type const * const operator -> () const;
+        bool operator != (iterator const &rhs) const;
+        reference operator * ();
+        const_reference operator * () const;
+        pointer_type operator -> ();
+        const_pointer_type operator -> () const;
         iterator& operator ++ (); // prefix
         iterator& operator ++ (int); // postfix
         iterator& operator -- (); // prefix
@@ -118,8 +130,48 @@ namespace r1
 
     private:
         // members:
-        list* m_List;
-        node_base* m_Node;
+        node_base *m_Node;
+    };
+
+    template <typename T>
+    class list<T>::const_iterator
+        : public std::iterator
+            <
+                std::bidirectional_iterator_tag,
+                typename list::value_type,
+                typename list::difference_type,
+                typename list::pointer_type,
+                typename list::reference
+            >
+    {
+    public:
+        // construct:
+        const_iterator();
+        const_iterator(const_iterator &&rhs);
+        const_iterator(const_iterator const &rhs);
+
+        // destroy:
+        ~const_iterator();
+
+        // assign:
+        const_iterator& operator = (const_iterator &&rhs);
+        const_iterator& operator = (const_iterator const &rhs);
+
+        // members:
+        bool operator == (const_iterator &&rhs) const;
+        bool operator == (const_iterator const &rhs) const;
+        bool operator != (const_iterator &&rhs) const;
+        bool operator != (const_iterator const &rhs) const;
+        const_reference operator * () const;
+        const_pointer_type operator -> () const;
+        const_iterator& operator ++ (); // prefix
+        const_iterator& operator ++ (int); // postfix
+        const_iterator& operator -- (); // prefix
+        const_iterator& operator -- (int); // postfix
+
+    private:
+        // members:
+        node_base *m_Node;
     };
 
     // non-member function overloads:
@@ -129,8 +181,6 @@ namespace r1
 #pragma region implementation
 
 #include <memory>
-
-#pragma endregion
 
 namespace r1
 {
@@ -163,5 +213,7 @@ namespace r1
 
 
 }
+
+#pragma endregion
 
 #endif // R1_LIST_HPP
